34a3b0d80bd7730d448eb71e137c0cee7c32cb4b
BugHunter-X <askzenai@gmail.com>
2025-01-12 09:41:16 +0100

    Add index.fixed.js file

---
+++ index.fixed.js
@@ -0,0 +1,11 @@
+```javascript
+// pages/index.js
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello, Next.js! (Fixed)</h1>
+      <p>This is the fixed version of the page, addressing routing issues.</p>
+    </div>
+  );
+}
+```
